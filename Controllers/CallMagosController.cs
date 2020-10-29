41ba5ae6fb78298ded9199d479f89722d5ef161e
ANOOP RANJAN <anooppriyaranjan@gmail.com>
2020-10-29 11:45:12 +0530

    CI  initiate

---
+++ Controllers/CallMagosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CallMagosWebApi.Controllers
+{
+    public class CallMagosController : Controller
+    {
+        // GET: CallMagos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
