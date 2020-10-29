dc0d9a4f8c4ff74212917153993d280b227dc18f
ANOOP RANJAN <anooppriyaranjan@gmail.com>
2020-10-29 12:02:38 +0530

    Call Magos Added

--- Controllers/CallMagosController.cs
+++ Controllers/CallMagosController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CallMagosWebApi.Controllers
 {
@@ -11,6 +7,7 @@ namespace CallMagosWebApi.Controllers
         // GET: CallMagos
         public ActionResult Index()
         {
+
             return View();
         }
     }
